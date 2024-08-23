1d96b02e14a866936fbdb9a684e7b7770a66147a
lipebarba2 <felipecinemkt@gmail.com>
2024-08-23 16:36:11 -0300

    feature configurando routes

---
+++ src/app/login/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const LoginPage = () => {
+  return (
+    <div className='flex w-full'>
+        <h1 className='flex justify-center items-center text-white
+        text-2xl'>Login
+        </h1>
+    </div>
+  )
+}
+
+export default LoginPage
---
+++ src/app/signup/signup.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const SignupPage = () => {
+  return (
+    <div className='flex'>
+      <h1 className='items-center text-white
+      text-2xl'> SignUp Page</h1>
+    </div>
+  )
+}
+
+export default SignupPage
